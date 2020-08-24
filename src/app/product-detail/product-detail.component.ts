c704d55299202b467b125ea83b48fe3faf8f81db
AmyTroutman <amyrtroutman@gmail.com>
2020-08-24 18:23:29 -0500

    added cart component

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { IProduct } from '../iproduct';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,15 +10,17 @@ import { IProduct } from '../iproduct';
 export class ProductDetailComponent implements OnInit {
 
   @Input() product: IProduct;
-  @Output() productAdded: EventEmitter<IProduct> = new EventEmitter<IProduct>();
-
-  constructor() { }
+  // @Output() productAdded: EventEmitter<IProduct> = new EventEmitter<IProduct>();
+  shoppingCart: IProduct [];
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.shoppingCart = this.productService.shoppingCart;
   }
 
   onProductAdded(): void {
-    this.productAdded.emit(this.product);
+    // this.productAdded.emit(this.product);
+    this.shoppingCart.push(this.product);
   }
 
 }
